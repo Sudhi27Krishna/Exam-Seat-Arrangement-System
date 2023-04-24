e5833cdbe9cbe707c4e374ce40f0ce498f1cde28
alimon777 <alimonna777@gmail.com>
2023-04-24 22:11:21 +0530

    trash icon added to university page

--- src/components/UeRow.jsx
+++ src/components/UeRow.jsx
@@ -1,4 +1,6 @@
 //row for exam time table
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function UeRow({ date, branch, slot, subject }) {
     return (
         <tr className="bg-gray-100">
@@ -7,7 +9,8 @@ export default function UeRow({ date, branch, slot, subject }) {
             <td className="text-center px-4 py-2">{branch}</td>
             <td className="text-center px-4 py-2">{slot}</td>
             <td className="text-center px-4 py-2">{subject}</td>
-            <td className="text-center px-4 py-2 rounded-tr-2xl rounded-br-2xl"><button className="text-red-500 hover:text-red-700">Delete</button></td>
+            <td className="text-center px-4 py-2 rounded-tr-2xl rounded-br-2xl"><button className="text-red-500 hover:text-red-700">
+                <FontAwesomeIcon icon={faTrashAlt} title="delete" /></button></td>
         </tr>
     );
 }
