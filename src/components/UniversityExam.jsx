943de332da747f64d783d5a7494a3431598dd42e
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2023-05-07 20:43:07 +0530

    Added adding and deleting exam schedule in university page with backend

--- src/components/UniversityExam.jsx
+++ src/components/UniversityExam.jsx
@@ -1,7 +1,7 @@
 import DropDownInput from './DropDownInput';
 import { useState, useRef, useEffect } from 'react';
 import Input from './Input';
-import uerow from '../uerow';
+// import uerow from '../uerow';
 import UeRow from './UeRow';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
@@ -19,14 +19,37 @@ export default function UniversityExam() {
     const subRef = useRef();
     const [subArray, setSubArray] = useState([]);
 
-    const handleExams = (e) => {
+    const handleSchedule = (e) => {
         e.preventDefault()
-        const newExam = { date: dateRef.current.value, time: timeRef.current.value, sem: Number(semRef.current.options[semRef.current.selectedIndex].value), branch: branchRef.current.value, slot: slotRef.current.value, subject: subRef.current.value };
-        const allExams = [...exams, newExam];
-        setExams(allExams);
-        console.log(allExams);
+        const newExam = { date: dateRef.current.value, time: timeRef.current.value, sem: Number(semRef.current.options[semRef.current.selectedIndex].value), branch: branchRef.current.value, slot: slotRef.current.value, subcode: subRef.current.value };
+        // const allExams = [...exams, newExam];
+        // setExams(allExams);
         formRef.current.reset();
         dateRef.current.focus();
+
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const postSchedule = async () => {
+            try {
+                await axiosPrivate.post(url, newExam, {
+                    signal: controller.signal
+                });
+                isMounted && setExams(prev => [...prev, newExam]);
+                console.log(exams);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        postSchedule();
+
+        semRef.current.focus();
+
+        return () => {
+            isMounted = false;
+            controller.abort();
+        }
     }
 
     const handleSlot = () => {
@@ -85,11 +108,47 @@ export default function UniversityExam() {
 
         getSubcode();
 
+        const getSchedule = async () => {
+            try {
+                const response = await axiosPrivate.get(url.concat("/schedule"), {
+                    signal: controller.signal
+                });
+                isMounted && setExams(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        isMounted && getSchedule();
+
         return () => {
             isMounted = false;
             controller.abort();
         }
-    },[axiosPrivate])
+    }, [axiosPrivate])
+
+    const handleDelete = (id) => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const deleteSchedule = async () => {
+            try {
+                await axiosPrivate.delete(url.concat(`/${id}`), {
+                    signal: controller.signal
+                });
+                isMounted && setExams(prev => prev.filter(item => item._id !== id));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        deleteSchedule();
+
+        return () => {
+            isMounted = false;
+            controller.abort();
+        }
+    }
 
     return (
         <div className="bg-background flex flex-col flex-grow">
@@ -116,7 +175,7 @@ export default function UniversityExam() {
 
             <div className="px-8 pt-6">
                 <h2 className="text-xl font-Outfit-Bold mb-8">ADD SLOTS</h2>
-                <form ref={formRef} className="flex flex-col st:flex-row justify-between" onSubmit={handleExams}>
+                <form ref={formRef} className="flex flex-col st:flex-row justify-between" onSubmit={handleSchedule}>
                     <Input input_id="date" title="Date" inputRef={dateRef} type="date" placeholder="09-09-2020" />
                     <DropDownInput input_id="time" title="Time" inputRef={timeRef} options={['FN', 'AN']} />
                     <DropDownInput input_id="branch" title="Branches" inputRef={branchRef} options={['CS', 'CC', 'CE', 'EC', 'EE', 'CA', 'ME']} isTarget handleSlot={handleSlot} />
@@ -134,6 +193,7 @@ export default function UniversityExam() {
                             <tr className="bg-grey-all font-Outfit-Bold">
                                 <th className="text-center px-4 py-2 rounded-tl-2xl rounded-bl-2xl"><span className="whitespace-nowrap">Date</span></th>
                                 <th className="text-center px-4 py-2"><span className="whitespace-nowrap">Time</span></th>
+                                <th className="text-center px-4 py-2"><span className="whitespace-nowrap">Semester</span></th>
                                 <th className="text-center px-4 py-2"><span className="whitespace-nowrap">Branch</span></th>
                                 <th className="text-center px-4 py-2"><span className="whitespace-nowrap">Slot</span></th>
                                 <th className="text-center px-4 py-2"><span className="whitespace-nowrap">Subject</span></th>
@@ -141,8 +201,7 @@ export default function UniversityExam() {
                             </tr>
                         </thead>
                         <tbody>
-                            {uerow.map(item => <UeRow key={item.id} date={item.date} time={item.time} branch={item.branch} slot={item.slot}
-                                subject={item.subject} />)}
+                            {exams.map(item => <UeRow key={item._id} id={item._id} date={item.date} time={item.time} sem={item.sem} branch={item.branch} slot={item.slot} subject={item.subcode} handleDelete={handleDelete} />)}
                         </tbody>
                     </table>
                 </div>
@@ -151,7 +210,7 @@ export default function UniversityExam() {
             <div className="px-8 py-4 mt-2">
                 <div className="flex flex-row justify-between items-center">
                     <div>
-                        <p className="font-Outfit-Regular">No of Exams scheduled : 7</p>
+                        <p className="font-Outfit-Regular">No of Exams scheduled : {exams.length}</p>
                     </div>
                     <div className="flex flex-row gap-10">
                         <button className="bg-gray-500 hover:bg-gray-400 text-white font-Outfit-Bold h-10 w-[10rem] rounded-[20px]" type="submit">CLEAR ALL</button>
