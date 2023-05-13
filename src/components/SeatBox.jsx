d2501cca04158d82be45b95db3fc2a87870451e9
alimon777 <alimonna777@gmail.com>
2023-05-13 09:33:28 +0530

    room selection color change to green

--- src/components/SeatBox.jsx
+++ src/components/SeatBox.jsx
@@ -1,9 +1,15 @@
-
+import { useState } from "react";
 export default function SeatBox({ room, capacity }) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleChange = () => {
+        setIsChecked(!isChecked);
+        console.log(room);
+    };
     return (
-        <div className="inline-grid place-items-center bg-grey-all w-16 h-16 border rounded-2xl m-2 py-2">
-            <p className="font-Outfit-Bold">{room}</p>
-            <p className="font-Outfit-Bold text-green-save">{capacity}</p>
-        </div>
+        <div className={`inline-grid place-items-center ${isChecked ? 'bg-green-save' : 'bg-grey-all'} w-16 h-16 border rounded-2xl m-2 py-2 cursor-pointer`} checked={isChecked} onClick={handleChange} >
+            <p className="font-Outfit-Bold cursor-pointer select-none">{room}</p>
+            <p className={`font-Outfit-Bold ${isChecked ? 'text-white' : 'text-green-save'} cursor-pointer select-none`}>{capacity}</p>
+        </div >
     );
 }
