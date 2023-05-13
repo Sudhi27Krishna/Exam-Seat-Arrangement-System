735571e98dd29e2a8dd40ff6e63d115908b3c335
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2023-05-13 10:40:54 +0530

    Created a list for selectedRooms

--- src/components/SeatBox.jsx
+++ src/components/SeatBox.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
-export default function SeatBox({ room, capacity }) {
+export default function SeatBox({ room, capacity, setSelectedRooms }) {
     const [isChecked, setIsChecked] = useState(false);
 
     const handleChange = () => {
         setIsChecked(!isChecked);
-        console.log(room);
+        if(!isChecked){
+            console.log(room);
+            setSelectedRooms(prev => [...prev, room]);
+        }
+        else{
+            console.log(room);
+            setSelectedRooms(prev => prev.filter(item => item !== room));
+        }
     };
     return (
         <div className={`inline-grid place-items-center ${isChecked ? 'bg-green-save' : 'bg-grey-all'} w-16 h-16 border rounded-2xl m-2 py-2 cursor-pointer`} checked={isChecked} onClick={handleChange} >
