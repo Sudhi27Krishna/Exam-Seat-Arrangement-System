1383064d514488b681869db6464f169f5833a101
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2023-05-27 21:49:52 +0530

    Added selected rooms for each date functionality

--- src/components/SeatBox.jsx
+++ src/components/SeatBox.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
-export default function SeatBox({ room, capacity, setSelectedRooms }) {
+export default function SeatBox({ room, capacity, setSelectedRooms, bookedRooms }) {
     const [isChecked, setIsChecked] = useState(false);
 
     const handleChange = () => {
         setIsChecked(!isChecked);
-        if(!isChecked){
+        if (!isChecked) {
             console.log(room);
             setSelectedRooms(prev => [...prev, room]);
         }
-        else{
+        else {
             console.log(room);
             setSelectedRooms(prev => prev.filter(item => item !== room));
         }
     };
     return (
-        <div className={`inline-grid place-items-center ${isChecked ? 'bg-green-save' : 'bg-grey-all'} w-16 h-16 border rounded-2xl m-2 py-2 cursor-pointer`} checked={isChecked} onClick={handleChange} >
+        <div className={`inline-grid place-items-center ${bookedRooms.length > 0 ? bookedRooms.includes(room) ? 'bg-red-500' : 'bg-grey-all'
+            : isChecked ? 'bg-green-save' : 'bg-grey-all'} w-16 h-16 border rounded-2xl m-2 py-2 cursor-pointer`} checked={isChecked} onClick={bookedRooms.length > 0 ? null : handleChange}>
             <p className="font-Outfit-Bold cursor-pointer select-none">{room}</p>
-            <p className={`font-Outfit-Bold ${isChecked ? 'text-white' : 'text-green-save'} cursor-pointer select-none`}>{capacity}</p>
+            <p className={`font-Outfit-Bold ${bookedRooms.length > 0 ? bookedRooms.includes(room) && 'bg-red-500' : isChecked ? 'text-white' : 'text-green-save'} cursor-pointer select-none`}>{capacity}</p>
         </div >
     );
 }
